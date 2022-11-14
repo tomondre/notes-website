5d959bc857b9718cde3f19ffe1375c5733ea0441
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-11-14 21:27:14 +0100

    .

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,5 @@
+variable "image_tag" {
+}
 module "notes_page" {
   source            = "git::https://github.com/tomondre/raspberry-kubernetes-cluster.git//terraform-modules/reusable-modules/deployment"
   service_name      = "notes-page"
@@ -5,4 +7,5 @@ module "notes_page" {
   health_check_path = "/"
   image_url         = "docker.io/tomondre/notes-page"
   port              = 80
+  image_tag         = var.image_tag
 }
